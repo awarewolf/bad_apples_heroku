7ea60d6d031db0859f02cba521ca7c71c0b3f096
Simon Pregent <simon.pregent@hec.ca>
2016-01-18 10:11:37 -0500

    Change mailer config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,15 +17,15 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => "localhost:3000" }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    domain: 'gmail.com',
-    user_name: 'simonpregent@gmail.com',
-    pasword: 'cpodls99',
-    authentication: 'plain',
-    enable_starttls_auto: true
-  }
+  # config.action_mailer.smtp_settings = {
+  #   address: 'smtp.gmail.com',
+  #   port: 587,
+  #   domain: 'gmail.com',
+  #   user_name: ENV["gmail_username"],
+  #   pasword: ENV["gmail_password"],
+  #   authentication: 'plain',
+  #   enable_starttls_auto: true
+  # }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
