aa2292f10b11d6c01fc6494c7a4d77d1b2799f82
Simon Pregent <simon.pregent@hec.ca>
2016-01-20 18:39:23 -0500

    Change mailgun credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,14 @@ Rails.application.configure do
   #   authentication: 'plain',
   #   enable_starttls_auto: true
   # }
+  config.action_mailer.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'bad-apples.herokuapp.com',
+  :authentication => :plain
+}
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
