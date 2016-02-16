f3cd1dc3c70b78fe177ed6ef33f25d5eec1a0e2c
Simon Pregent <simon.pregent@hec.ca>
2016-02-16 14:55:01 -0500

    Implement changes made during outage

--- app/models/concerns/tmdbapi.rb
+++ app/models/concerns/tmdbapi.rb
@@ -3,12 +3,12 @@ module Tmdbapi
 
   attr_reader :get_configuration, :images, :base_url, :poster_size
 
-  POSTER_SIZE = 6 # See http://docs.themoviedb.apiary.io {6 for original}
+  POSTER_SIZE_INDEX = 1 # See http://docs.themoviedb.apiary.io {6 for original}
 
   included do
-    @@configuration = self.get_configuration
-    @@base_url = self.images('base_url')
-    @@poster_size = "#{@@base_url}#{self.images('poster_sizes', POSTER_SIZE)}"
+      CONFIGURATION = self.get_configuration
+      BASE_URL = self.images('base_url')
+      POSTER = "#{BASE_URL}#{poster_size(POSTER_SIZE_INDEX)}"
   end
 
   # methods defined here are going to extend the class, not the instance of it
@@ -26,10 +26,14 @@ module Tmdbapi
 
     def images(parameter, *index)
       if index.empty?
-        @@configuration.images.send(parameter.to_sym)
+        CONFIGURATION.images.send(parameter.to_sym)
       else
-        @@configuration.images.send(parameter.to_sym[index[0]])
+        CONFIGURATION.images.send(parameter.to_sym)[index[0]]
       end
+    end
+
+    def poster_size(index)
+      self.images('poster_sizes', index)
     end
   end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,8 @@ class Movie < ActiveRecord::Base
 
   include Tmdbapi
 
+  before_create :add_poster_image_url
+
   scope :search, ->(query) {where("title like ? OR director like ? OR description like ?", "%#{query}%", "%#{query}%", "%#{query}%")}
 
   has_many :reviews
@@ -34,6 +36,14 @@ class Movie < ActiveRecord::Base
 
   def latest_reviews(count=3)
     reviews.order(:created_at).limit(count).includes(:user)
+  end
+
+  private
+
+  def add_poster_image_url
+    results = Movie.find_by_title(self.title)
+    return nil if results.empty?
+    self.poster_image_url = "#{Movie::POSTER}#{results.first.poster_path}"
   end
 
 end
